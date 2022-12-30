7fd3132babf0f3aab0092065ce3ee0c0cf28fae3
Iksinski <creeperrwtf@gmail.com>
2022-12-30 16:18:00 +0100

    HealthChecks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Net.Mime;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -11,11 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
+var mongodbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 builder.Services.AddSingleton<IMongoClient>(
     ServiceProvider => {
-        var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-        return new MongoClient(settings.ConnectionString);
+        return new MongoClient(mongodbSettings.ConnectionString);
     }
 );
 builder.Services.AddSingleton<IItemsRepository,MongoDbItemsRepository>();
@@ -27,9 +29,39 @@ builder.Services.AddControllers(
 );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongodbSettings.ConnectionString,
+        name:"mongodb",
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[]{"db"}
+    );
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapHealthChecks("/hc/db", new HealthCheckOptions{
+    Predicate = (check) => check.Tags.Contains("db"),
+    ResponseWriter = async (context,report) => {
+        var result = JsonSerializer.Serialize(
+            new{
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new{
+                    name = entry.Key,
+                    status = entry.Value.Status.ToString(),
+                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                    duration = entry.Value.Duration.ToString()
+                })
+            }
+        );
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
+app.MapHealthChecks("/hc/live", new HealthCheckOptions{
+    Predicate = (_) => false
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,3 +77,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+//docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=-e MONGO_INITDB_ROOT_PASSWORD= mongo
--- obj/Debug/net7.0/Project.AssemblyInfo.cs
+++ obj/Debug/net7.0/Project.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2bb53a93-2cd5-4abe-a218-cbb15aebedf7")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
